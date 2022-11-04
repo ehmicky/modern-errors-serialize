ee29b7ccff783ff67fc2ca81064bb81a3aae23bb
ehmicky <ehmicky@gmail.com>
2022-11-04 21:14:08 +0100

    Rename repository

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-export default function templateName(value) {
+export default function modernErrorsSerialize(value) {
   return value
 }
--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 import test from 'ava'
-import templateName from 'template-name'
+import modernErrorsSerialize from 'modern-errors-serialize'
 
 test('Dummy test', (t) => {
-  t.true(templateName(true))
+  t.true(modernErrorsSerialize(true))
 })
