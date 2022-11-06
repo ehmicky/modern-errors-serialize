5ab542af22651c008b7a7e2b22cb888e175ec198
ehmicky <ehmicky@gmail.com>
2022-11-06 23:52:03 +0100

    Use node protocol

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -1,4 +1,4 @@
-import { runInNewContext } from 'vm'
+import { runInNewContext } from 'node:vm'
 
 import modernErrors from 'modern-errors'
 import modernErrorsSerialize from 'modern-errors-serialize'
