4b4f8e73192d130f81a77afe06acdabd1b35d711
ehmicky <ehmicky@gmail.com>
2022-11-13 17:38:22 +0100

    Add comment

--- src/main.js
+++ src/main.js
@@ -10,6 +10,18 @@ const toJSON = function ({ error, instancesData }) {
 
 // Plugin options are kept in some undocumented `WeakMap` called `instancesData`
 // Those are kept by serializing them as a `pluginsOpts` property.
+// Even though `error-serializer` allows keeping constructor arguments using
+// `error.constructorArgs`, we do not use it because it is too difficult to
+// implement and brittle. It would require:
+//  - Serializing `cause` and aggregate `errors` which are
+//    - Big
+//    - Mutable
+//    - Difficult not to skip since `plugin.properties()` and `custom` behavior
+//      might depend on their presence
+// - Storing `constructorArgs` as error property due to `error-serializer` being
+//   deep and not calling `error.toJSON()`
+// - Users passing initial constructor arguments down, which is complicated when
+//   they also want to modify them
 const beforeSerialize = function (error, instancesData) {
   const { pluginsOpts } = instancesData.get(error)
 
