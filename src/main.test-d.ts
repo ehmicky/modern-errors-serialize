6e5ad1c0e7066dfc9f4078dbad55f9680ce1e0da
ehmicky <ehmicky@gmail.com>
2022-12-04 20:05:05 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,9 +1,4 @@
-import {
-  expectType,
-  expectAssignable,
-  expectNotAssignable,
-  expectError,
-} from 'tsd'
+import { expectType, expectAssignable, expectNotAssignable } from 'tsd'
 
 import ModernError, { ErrorInstance } from 'modern-errors'
 import modernErrorsSerialize, {
@@ -17,9 +12,12 @@ const BaseError = ModernError.subclass('BaseError', {
 const error = new BaseError('')
 const errorObject = BaseError.serialize(error)
 
-expectError(BaseError.serialize(error, undefined))
-expectError(error.toJSON(undefined))
-expectError(BaseError.parse(errorObject, undefined))
+// @ts-expect-error
+BaseError.serialize(error, undefined)
+// @ts-expect-error
+error.toJSON(undefined)
+// @ts-expect-error
+BaseError.parse(errorObject, undefined)
 
 expectType<ErrorObject>(BaseError.serialize(error))
 expectType<ErrorObject>(BaseError.serialize({ error }))
@@ -31,15 +29,20 @@ expectType<ErrorInstance>(BaseError.parse(errorObject))
 expectType<ErrorInstance>(BaseError.parse({ errorObject }))
 
 BaseError.subclass('TestError', { serialize: { loose: true } })
-expectError(BaseError.subclass('TestError', { serialize: true }))
+// @ts-expect-error
+BaseError.subclass('TestError', { serialize: true })
 new BaseError('', { serialize: { loose: true } })
-expectError(new BaseError('', { serialize: { loose: 'true' } }))
+// @ts-expect-error
+new BaseError('', { serialize: { loose: 'true' } })
 BaseError.serialize(error, { loose: true })
-expectError(BaseError.serialize(error, { loose: 'true' }))
+// @ts-expect-error
+BaseError.serialize(error, { loose: 'true' })
 error.toJSON({ loose: true })
-expectError(error.toJSON({ loose: 'true' }))
+// @ts-expect-error
+error.toJSON({ loose: 'true' })
 BaseError.parse(errorObject, { loose: true })
-expectError(BaseError.parse(errorObject, { loose: 'true' }))
+// @ts-expect-error
+BaseError.parse(errorObject, { loose: 'true' })
 
 expectAssignable<Options>({})
 expectNotAssignable<Options>(true)
